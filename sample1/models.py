e901509eb505beee5b5bcc623de08d2f94c02973
pouria eini <pouria.eini@eskano.com>
2016-07-05 10:33:59 +0430

    informatin model added. information model str test added

--- sample1/models.py
+++ sample1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Information(models.Model):
+    full_name = models.CharField(max_length=64)
+    create_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        if self.full_name and not self.full_name == '':
+            return self.full_name
+        else:
+            return 'The man'
--- sample1/tests.py
+++ sample1/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sample1.models import Information
+
+
+class InformationTestCase(TestCase):
+    def test_information_str_with_full_name(self):
+        information = Information(full_name='someone')
+
+        self.assertEqual('someone', str(information))
+
+    def test_information_str_without_full_name(self):
+        information = Information()
+
+        self.assertEqual('The man', str(information))
+
+    def test_information_str_with_empty_full_name(self):
+        information = Information(full_name='')
+
+        self.assertEqual('The man', str(information))
